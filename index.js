7816b21aca9c108a389dc0bca04fb73ff27c8344
Emily <emilybribragg@gmail.com>
2021-11-25 09:35:44 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+//generate a number that is two numbers multiplied
+let num1=31
+let num2=2
+let multiply = num1 * num2
+//generate a random number
+const random=Math.floor(Math.random() * 10) + 1;
+
+let num3=16
+let num4=4
+let mod = num3 / num4
+
+
+const max=Math.max(5, 10, 15, 20);
